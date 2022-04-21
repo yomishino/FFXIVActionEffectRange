19e5861746b673cfc0c1263a5c6a2535b2d30644
yomishino <29257841+yomishino@users.noreply.github.com>
2022-04-21 20:46:10 +1000

    Add #29412 and #29551 to be ruled out

--- src/Actions/Data/Predefined/RuledOutActions.cs
+++ src/Actions/Data/Predefined/RuledOutActions.cs
@@ -6,7 +6,10 @@
         public static uint[] PredefinedList => new uint[]
         {
             2262,   // Shukuchi (NIN)
+
+            29412,   // Bishop Autoturret (MCH PvP)
             29513,   // Shukuchi (NIN PvP)
+            29551,   // Regress (RPR PvP)
 
             11401,  // Loom (BLU)
 
